72f41dce3d0935f7118496b5b5e25f741e7a4e2d
programcode200 <rohans1021@gmail.com>
2025-03-06 15:18:01 +0530

    bugs

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -1,29 +1,13 @@
-// import axios from "axios";
-// import {BASE_URL} from "../constants.js"
-
-// const axiosInstance = axios.create();
-
-// axiosInstance.defaults.baseURL = BASE_URL;
-// axiosInstance.defaults.withCredentials = true;
-
-// export default axiosInstance;
-
-
 import axios from "axios";
+import {BASE_URL} from "../constants.js"
 
-const axiosInstance = axios.create({
-  baseURL: "https://backend-ten-sooty-67.vercel.app/api/v1",
-  withCredentials: true, // ✅ Ensures cookies are sent with requests
-});
+const axiosInstance = axios.create();
 
-// Attach token to all requests
-axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token"); // ✅ Get token from localStorage
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+axiosInstance.defaults.baseURL = BASE_URL;
+axiosInstance.defaults.withCredentials = true;
 
 export default axiosInstance;
+
+
+
 
