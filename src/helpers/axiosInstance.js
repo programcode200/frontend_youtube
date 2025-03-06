5139da53c9d8ab4c06864ca24e62473f530cfeab
programcode200 <rohans1021@gmail.com>
2025-03-06 20:03:26 +0530

    bugs

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 import {BASE_URL} from "../constants.js"
 
-const axiosInstance = axios.create();
+const axiosInstance = axios.create({
+    baseURL : BASE_URL,
+    withCredentials : true
+});
 
-axiosInstance.defaults.baseURL = BASE_URL;
-axiosInstance.defaults.withCredentials = true;
 
 export default axiosInstance;
 
