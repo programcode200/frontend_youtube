65106382d89a0118b2786f2265b90007c6dac2f4
programcode200 <rohans1021@gmail.com>
2025-03-09 19:32:13 +0530

    update

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -10,7 +10,7 @@ function Logo({ size = "30" }) {
                     size={size}
                     color="#A855F7"
                 />
-                <span className="font-bold text-white">STREAMIFY</span>
+                <span className="font-bold text-white">STREAM-X</span>
             </Link>
         </>
     );
